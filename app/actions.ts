0cf77dc39568219be810970aca0f5ed31aff1968
Halldor <halldor.hrafn.reynisson@gmail.com>
2025-03-05 00:34:01 +0000

    Ghost changes made I have no idea where the change is to this file

--- app/actions.ts
+++ app/actions.ts
@@ -2,7 +2,7 @@
 
 import { encodedRedirect } from "@/utils/utils";
 import { createClient } from "@/utils/supabase/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const signUpAction = async (formData: FormData) => {
